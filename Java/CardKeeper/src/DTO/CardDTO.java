2c823058615f4f26a49a87aea116a84cfbb45f89
GabrielMikas <112135725+GabrielMikas@users.noreply.github.com>
2023-06-06 22:11:02 -0300

    Initial commit

---
+++ Java/CardKeeper/src/DTO/CardDTO.java
@@ -0,0 +1,31 @@
+package DTO;
+
+public class CardDTO {
+    String cardCode;
+    String cardName;
+    int cardNumber;
+
+    public String getCardCode() {
+        return cardCode;
+    }
+
+    public void setCardCode(String cardCode) {
+        this.cardCode = cardCode;
+    }
+
+    public String getCardName() {
+        return cardName;
+    }
+
+    public void setCardName(String cardName) {
+        this.cardName = cardName;
+    }
+
+    public int getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(int cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+}
