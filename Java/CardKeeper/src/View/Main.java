79b52dc3420dfcc62423a7a9c8fae6e9a5004519
GabrielMikas <112135725+GabrielMikas@users.noreply.github.com>
2023-06-09 20:56:56 -0300

    090623final

--- Java/CardKeeper/src/View/Main.java
+++ Java/CardKeeper/src/View/Main.java
@@ -10,25 +10,32 @@ import java.awt.event.ActionListener;
 public class Main {
     public static void main(String[] args) {
         //main frame
+
         JFrame Jframe = new JFrame();
         JButton testjb = new JButton("Register");
         testjb.setBounds(50,400,95,30);
+        JTextField jt1 = new JTextField("This is jt1(cardName)");
+        jt1.setBounds(50,250,150,25);
+        JTextField jt2 = new JTextField("This is jt2(cardCode)");
+        jt2.setBounds(50,300,150,25);
+        JTextField jt3 = new JTextField("This is jt3(cardAmount)");
+        jt3.setBounds(50,350,150,25);
         //gets input from text fields and prints them
 
-        TextFieldGroup tfg = new TextFieldGroup();
+
 
         Jframe.setSize(400,500);
+        Jframe.add(jt1);
+        Jframe.add(jt2);
+        Jframe.add(jt3);
         Jframe.add(testjb);
-        Jframe.add(tfg.cardNameInput());
-        Jframe.add(tfg.cardAmountInput());
-        Jframe.add(tfg.cardCodeInput());
 
-        JTextField cardNameField = tfg.cardNameInput();
+
         testjb.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                System.out.println(cardNameField.getText());
-                System.out.println(tfg.cardCodeInput().getText());
-                System.out.println(tfg.cardAmountInput().getText());
+                System.out.println(jt1.getText());
+                System.out.println(jt2.getText());
+                System.out.println(jt3.getText());
             }
         });
 
