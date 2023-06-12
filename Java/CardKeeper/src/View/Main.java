37947c6587ea5074455547aa21e41075716d44e0
GabrielMikas <112135725+GabrielMikas@users.noreply.github.com>
2023-06-12 18:39:40 -0300

    120623FINAL
    DB amount type change INTEGER -> TEXT
    Added JOptionPane for when input fills are empty

--- Java/CardKeeper/src/View/Main.java
+++ Java/CardKeeper/src/View/Main.java
@@ -15,7 +15,7 @@ public class Main {
         String data[][]={{"ID", "Name", "Code", "Amount"}};
         String col[]={"ID", "Name", "Code", "Amount"};
 
-
+        //Initializing components
         DefaultTableModel model = new DefaultTableModel(data, col);
         JFrame Jframe = new JFrame();
         JButton testjb = new JButton("Register");
@@ -23,10 +23,11 @@ public class Main {
         JLabel l2 = new JLabel("Card Code");
         JLabel l3 = new JLabel("Amount of cards");
         JTable jt = new JTable(model);
-        JTextField jt1 = new JTextField("This is jt1(cardName)");
-        JTextField jt2 = new JTextField("This is jt2(cardCode)");
-        JTextField jt3 = new JTextField("This is jt3(cardAmount)");
+        JTextField jt1 = new JTextField();
+        JTextField jt2 = new JTextField();
+        JTextField jt3 = new JTextField();
 
+        //Setting component bounds
         jt.setBounds(225,30,400,400);
         l1.setBounds(50,30,150,25);
         l2.setBounds(50,80,150,25);
@@ -36,7 +37,7 @@ public class Main {
         jt3.setBounds(50,150,150,25);
         testjb.setBounds(50,200,95,30);
 
-
+        //Adding stuff to the main frame
         Jframe.setSize(800,500);
         Jframe.add(jt);
         Jframe.add(l1);
@@ -47,7 +48,7 @@ public class Main {
         Jframe.add(jt3);
         Jframe.add(testjb);
 
-
+        //Action listeners
         testjb.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
 
@@ -57,18 +58,24 @@ public class Main {
                 String name = jt1.getText();
                 String code = jt2.getText();
                 String amount = jt3.getText();
-                cardDTOobject.setCardName(name);
-                cardDTOobject.setCardCode(code);
-                cardDTOobject.setCardNumber(amount);
+                if(name.equals("")||code.equals("")||amount.equals("")){
+                    JOptionPane.showMessageDialog(Jframe,"Please fill all the fields");
+                }
+                else{
+                    cardDTOobject.setCardName(name);
+                    cardDTOobject.setCardCode(code);
+                    cardDTOobject.setCardNumber(amount);
 
-                cardDAOobject.registerTest(cardDTOobject);
+                    cardDAOobject.registerTest(cardDTOobject);
 
 
-                //Adds the registered card to the table
-                String data[] = {"ID", name, code, amount};
-                model.addRow(data);
+                    //Adds the registered card to the table
+                    String data[] = {"ID", name, code, amount};
+                    model.addRow(data);
 
-                JOptionPane.showMessageDialog(Jframe,"Card registered successfully");
+                    JOptionPane.showMessageDialog(Jframe,"Card registered successfully");
+                }
+
 
             }
         });
