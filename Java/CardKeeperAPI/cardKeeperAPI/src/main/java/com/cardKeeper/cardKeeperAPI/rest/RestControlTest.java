ac32fc6c4f7b423aaa2cfc43ef6b140952b2674e
GabrielMikas <112135725+GabrielMikas@users.noreply.github.com>
2023-06-27 16:23:09 -0300

    NEURON ACTIVATION

--- Java/CardKeeperAPI/cardKeeperAPI/src/main/java/com/cardKeeper/cardKeeperAPI/rest/RestControlTest.java
+++ Java/CardKeeperAPI/cardKeeperAPI/src/main/java/com/cardKeeper/cardKeeperAPI/rest/RestControlTest.java
@@ -1,25 +1,14 @@
 package com.cardKeeper.cardKeeperAPI.rest;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("cards")
 public class RestControlTest {
-    @GetMapping("/")
-    public String HelloWorld(){
-        return "Hello World";
-    }
-    @GetMapping("/cardID")
-    public String CardIDs(){
-        return "Card ID: 0000";
-    }
-    @GetMapping("/cardCode")
-    public String CardCodes(){
-        return "Card Code: MP22-22";
-    }
-    @GetMapping("/cardName")
-    public String CardNames(){
-        return  "Card name: Nibiru";
+
+    @PostMapping
+    public void ReceiveCards(@RequestBody cardsDTO objCardsDTO){
+        System.out.println(objCardsDTO);
     }
 
 }
---
+++ Java/CardKeeperAPI/cardKeeperAPI/src/main/java/com/cardKeeper/cardKeeperAPI/rest/cardsDTO.java
@@ -0,0 +1,4 @@
+package com.cardKeeper.cardKeeperAPI.rest;
+
+public record cardsDTO(String cardName, String cardCode, String cardQual, String cardAmount) {
+}
