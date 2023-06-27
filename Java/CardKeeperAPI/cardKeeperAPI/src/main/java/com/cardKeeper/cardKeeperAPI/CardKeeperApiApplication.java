084eb08ce11b133508603ee8c7a9c28587b0149b
GabrielMikas <112135725+GabrielMikas@users.noreply.github.com>
2023-06-27 13:05:40 -0300

    starting spring project

---
+++ Java/CardKeeperAPI/cardKeeperAPI/src/main/java/com/cardKeeper/cardKeeperAPI/CardKeeperApiApplication.java
@@ -0,0 +1,13 @@
+package com.cardKeeper.cardKeeperAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardKeeperApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardKeeperApiApplication.class, args);
+	}
+
+}
---
+++ Java/CardKeeperAPI/cardKeeperAPI/src/test/java/com/cardKeeper/cardKeeperAPI/CardKeeperApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardKeeper.cardKeeperAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardKeeperApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
